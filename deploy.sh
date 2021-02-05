644b3e5dfb1b7abcae9525b2a0a8db5f52a8d9c6
Remo Oehninger <remo.oehninger@stud.hslu.ch>
2021-02-05 09:10:32 +0100

    Geändert

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
 ## ##################################################
+## set config
+cd $(dirname $0)
+source ./config.sh
+
+
+## ##################################################
 ## create hdfs directories
 hdfs dfs -test -f flights_2 || hdfs dfs -mkdir -p /user/${USER}/flights_2
 
